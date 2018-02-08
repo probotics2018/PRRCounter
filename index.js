5a63e7dd403e6b0172be863ef18b5c9a4b690cc9
rycao18 <rycao18@gmail.com>
2018-02-08 13:56:45 -0600

    here

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = robot => {
+  robot.on('pull_request.opened', async context => {
+    const options = context.repo({path: 'R_ISSUE_REPLY_TEMPLATE.md'})
+    const res = await context.github.repos.getContent(options)
+    const template = Buffer.from(res.data.content, 'base64').toString()
+    console.log("SOMETHING IS HAPPENING");
+
+    return context.github.issues.createComment(context.issue({body: template}))
+  });
+}
