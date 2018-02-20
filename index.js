af5e3d025276931e237bc9f748296639aaee7170
rycao18 <rycao18@gmail.com>
2018-02-20 13:48:19 -0600

    added another console.log

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ module.exports = robot => {
     const res = await context.github.repos.getContent(options)
     const template = Buffer.from(res.data.content, 'base64').toString()
     console.log("SOMETHING IS HAPPENING");
+    console.log(context.github.gitdata.getAllReferences());
+    //console.log(context.github.statuses.get())
 
 
     // github.pull_request doesn't exist
 
-    return context.github.pullRequests.createComment(context.issue({body: template}));
+    return context.github.issues.createComment(context.issue({body: template}));
   });
 }
