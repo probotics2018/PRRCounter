f6498bd87350241e4aa6b109b84a7e87f0dc0252
helenagao14 <hygao@stu.naperville203.org>
2018-02-20 14:12:22 -0600

    2/20

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+module.exports = robot => {
+  robot.on('push', async context => {
+    // Code was pushed to the repo, what should we do with it?
+    robot.log(context)
+  })
+}
+
+
+
+const MIN_REQUIRED_APPROVES = 2; 
+const secret = 'GITHUB_SECRET_KEY_FOR_WEBHOOK';
+
+function countNumberofApprovals(pullRequestReviews)
+
+let countApproved = 0;
+let finalStatusOfApproval = {};
+
+
+
+
 approve=require("./util/approveUtil");
 
