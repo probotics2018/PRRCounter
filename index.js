54c26a63886e9fd1811043b6b938e1eaea5feeeb
rycao18 <rycao18@gmail.com>
2018-02-22 14:11:15 -0600

    here

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const numApproved = 0;
 
+const owner = "probotics2018";
+const repo = "test";
+
 module.exports = robot => {
 	var singlePR = {};
 	robot.on('pull_request.opened', async context => {
@@ -10,12 +13,12 @@ module.exports = robot => {
 	    console.log("SOMETHING IS HAPPENING");
 	    // Array of all pull requests and getting the first object in the array
 	    var prArray = await context.github.pullRequests.getAll({
-			"owner": "probotics2018",
-			"repo": "test"
+			"owner": owner
+			"repo": repo
 		});
 		// Should return the latest pull request
 		singlePR = JSON.parse(JSON.stringify(prArray))["data"][0];
-
+		setStatus = await context.github.repos.createStatus({owner, repo, sha, state, target_url, description, context})
 
 	    return context.github.issues.createComment(context.issue({body: template}));
 	});
@@ -23,11 +26,12 @@ module.exports = robot => {
 	console.log('Pull request submitted');
 		approveReviews = 0;
 	  	var reviewsArray = await context.github.pullRequests.getReviews({
-	  		"owner": "probotics2018",
-	  		"repo": "test",
+	  		"owner": owner,
+	  		"repo": repo,
 	  		"number": singlePR["number"]
 	  	});
 	  	if (reviewsArray.length > numApproved) {
+	  		console.log("we're in the loop");
 	  		for (var a = 0; a < reviewsArray; a++) {
 	  			console.log(reviewArrays[a]);
 	  			console.log(reviewArrays[a]["state"]);
