c82892e005eae0bba34f13e1b4c3cc25be08097d
helenagao14 <hygao@stu.naperville203.org>
2018-02-25 18:04:58 -0600

    to/25

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
 module.exports = robot => {
   robot.on('pull_request.opened', async context => {
     // Code was pushed to the repo, what should we do with it?
-    const result = await context.github.pullRequests.getReview(
-   )
+    const result = await context.github.pullRequests.getReview({
+owner:context.github.pullRequests.head.repo.owner.login,
+repo:context.github.pullRequests.head.repo.name,
+number:context.github.pullRequests.number
+})
+
+   #start to count the number of approves
+   const config = await context.config('PRRCounter.yml')
+   
+   let countApproved = 0;
+   pullRequestReviews.forEach(element in result)
+   if(element.state === "APPROVED"){
+      countApproved =countApproved + 1;
+   }
+
+   #now we have the count
+let Status;
+let Description;
+
+   if (countApproved <config.MIN_REQUIRED_APPROVES){
+	Status="success";
+        Description="Ready for Merge";
+   }
+   else{
+	Status="pending";
+        Description="Need two approvals for Merge";
+   }
+
+#POST /repos/:owner/:repo/statuses/:sha
+
+context.github.repos.createStatus({
+owner:context.github.pullRequests.head.repo.owner.login,
+repo:context.github.pullRequests.head.repo.name,
+sha:context.github.pullRequests.head.sha,
+state:Status,
+description:Description
+}).then(()=>context.github.response.send(Status))
 
 
 
-    robot.log(context)
+   }
+    
+   robot.log(context)
   })
 }
 
 
 
 
-
-const MIN_REQUIRED_APPROVES = 2; 
-const secret = 'GITHUB_SECRET_KEY_FOR_WEBHOOK';
-
-function countNumberofApprovals(pullRequestReviews)
-
-let countApproved = 0;
-let {};
-pullRequestReviews.forEach(function(element)))
-    if(element.state === "APPROVED"){
-            countApproved = 1;
-        }
-    else if(element.state === "CHANGES_REQUESTED" || element.status === "DISMISSED"){
-            countApproved = 0;
-        }
-    else{
-            countApproved = 0;
-
-
-
-
-approve=require("./util/approveUtil");
 
--- util/approveUtil.js
+++
@@ -1,7 +0,0 @@
-const countApprove /* : Listener */ = async context => {
-return 1;
-};
-
-module.exports = {
-  countApprove,
-};
