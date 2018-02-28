271849ad38bfabc9f339cadd0a835d135b188e98
helenagao14 <hygao@stu.naperville203.org>
2018-02-27 20:14:59 -0600

    2/27

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = robot => {
-	robot.on('pull_request.submitted', async context => {
+	robot.on('pull_request.opened', async context => {
     // Code was pushed to the repo, what should we do with it?
     console.log("Catch submit event");
     console.log(context.github.pullRequests.head.repo.owner.login);
