2129350cd36bddb998428122ef68b5a72df89660
helenagao14 <hygao@stu.naperville203.org>
2018-02-27 19:48:56 -0600

    2/27

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 module.exports = robot => {
 	robot.on('pull_request.submitted', async context => {
     // Code was pushed to the repo, what should we do with it?
+    console.log("Catch submit event");
+    console.log(context.github.pullRequests.head.repo.owner.login);
+    console.log(context.github.pullRequests.head.repo.name);
+    console.log(context.github.pullRequests.number);
     const result = await context.github.pullRequests.getReview({
     	owner:context.github.pullRequests.head.repo.owner.login,
     	repo:context.github.pullRequests.head.repo.name,
     	number:context.github.pullRequests.number
     })
-
+    console.log(result);
    //start to count the number of approves
    const config = await context.config('PRRCounter.yml')
    
    let countApproved = 0;
    pullRequestReviews.forEach(element in result)
+   {
+   console.log(element) 
    if(element.state === "APPROVED"){
    	countApproved =countApproved + 1;
    }
+}
 
-   //now we have the count
+   //now we have the councontext.github.pullRequests.head.repo.namet;
+
    let Status;
    let Description;
+
+   console.log(countApproved);
 
    if (countApproved <config.MIN_REQUIRED_APPROVES){
    	Status="success";
