b2b466533595f9f97fcf69a12392b06b8124e7b6
Saanvi Dhingra <sdhingra@stu.naperville203.org>
2018-02-28 10:29:55 -0600

    new

--- index.js
+++ index.js
@@ -1,12 +1,79 @@
+const fs = require('fs');
+const numApproved = 0;
+
+const owner = "probotics2018";
+const repo = "test";
+
 module.exports = robot => {
-  robot.on('issues.opened', async context => {
-    const options = context.repo({path: 'R_ISSUE_REPLY_TEMPLATE.md'})
-    const res = await context.github.repos.getContent(options)
-    const template = Buffer.from(res.data.content, 'base64').toString()
+	var singlePR = {};
+	robot.on('pull_request.opened', async context => {
+	  	const options = context.repo({path: 'R_ISSUE_REPLY_TEMPLATE.md'})
+	    const res = await context.github.repos.getContent(options)
+	    const template = Buffer.from(res.data.content, 'base64').toString()
+	    console.log("SOMETHING IS HAPPENING");
+	    // Array of all pull requests and getting the first object in the array
 
-     const result = await github.issues.pullRequests.getAll(saanvid, PRRcounter)
+		const prArray = await context.github.pullRequests.getAll({
+			"owner": owner,
+			"repo": repo,
+			"state":"open"
+		});
+		// return the latest pull request
+		singlePR = JSON.parse(JSON.stringify(prArray))["data"][0];
+		console.log("SINGLEPRNUMBER" + singlePR["number"]);
+		var commitArray = await context.github.pullRequests.getCommits({
+			"owner": owner, 
+			"repo": repo, 
+			"number": singlePR["number"]
+		});
+		commitArray = JSON.parse(JSON.stringify(commitArray));
+		console.log("COMMITARRAY" + JSON.stringify(commitArray));
+		var sha = commitArray["data"][0]["sha"];
+		var commitURL = commitArray["data"][0]["url"];
+		if (sha == null) {
+			console.log("WHYYYYYYYYYYYYYYYYY");
+		} else {
+			setStatus = await context.github.repos.createStatus({
+				"owner": owner, 
+				"repo": repo,
+				"sha": sha, 
+				"state": "failure"
+			});
+		}
 
-    return context.github.issues.createComment(context.issue({body: template}))
-    //get all pull requests
-  })
+	    return context.github.issues.createComment(context.issue({body: template}));
+	});
+	robot.on('pull_request_review.submitted', async context => {
+	console.log('Review submitted');
+		approveReviews = 0;
+	  	var reviewsArray = await context.github.pullRequests.getReviews({
+	  		"owner": owner,
+	  		"repo": repo,
+	  		"number": singlePR["number"]
+	  	});
+	  	reviewsArray = JSON.parse(JSON.stringify(reviewsArray));
+	  	reviewsArray = reviewsArray["data"];
+	  	console.log("REVIEWSARRAY" + JSON.stringify(reviewsArray));
+	  	if (reviewsArray.length > numApproved) {
+	  		console.log("we're in the loop");
+	  		for (var a = 0; a < reviewsArray.length; a++) {
+	  			console.log(reviewsArray[a]);
+	  			console.log(reviewsArray[a]["state"]);
+	  			if(reviewsArray[a]["state"] == "APPROVED") {
+	  				approveReviews++;
+	  			}
+	  		}
+	  	}
+	  	if (approveReviews > numApproved) {
+	  		console.log("There are more than 2 approved reviews");
+	  		setStatus = await context.github.repos.createStatus({
+	  			"owner":owner,
+	  			"repo":repo,
+	  			"sha":sha,
+	  			"state": "success"
+	  		})
+	  	} else {
+	  		console.log("There are " + approveReviews + " approved reviews");
+	  	}
+	});
 }
---
+++ index2.js
@@ -0,0 +1,60 @@
+module.exports = robot => {
+	robot.on('pull_request.opened', async context => {
+    // Code was pushed to the repo, what should we do with it?
+    console.log("Catch submit event");
+    console.log("1:"+context.github.pullRequests);
+    console.log("2:"+context.github.pullRequests.head);
+    console.log("3:"+context.github.pullRequests.head.repo);
+    console.log("4:"+context.github.pullRequests.head.repo.owner.login);
+    console.log(context.github.pullRequests.head.repo.name);
+    console.log(context.github.pullRequests.number);
+    const result = await context.github.pullRequests.getReview({
+    	owner:context.github.pullRequests.head.repo.owner.login,
+    	repo:context.github.pullRequests.head.repo.name,
+    	number:context.github.pullRequests.number
+    })
+    console.log(result);
+   //start to count the number of approves
+   const config = await context.config('PRRCounter.yml')
+   
+   let countApproved = 0;
+   pullRequestReviews.forEach(element in result)
+   {
+   console.log(element) 
+   if(element.state === "APPROVED"){
+   	countApproved =countApproved + 1;
+   }
+}
+
+   //now we have the councontext.github.pullRequests.head.repo.namet;
+
+   let Status;
+   let Description;
+
+   console.log(countApproved);
+
+   if (countApproved <config.MIN_REQUIRED_APPROVES){
+   	Status="success";
+   	Description="Ready for Merge";
+   }
+   else{
+   	Status="pending";
+   	Description="Need two approvals for Merge";
+   }
+
+//POST /repos/:owner/:repo/statuses/:sha
+
+context.github.repos.createStatus({
+	owner:context.github.pullRequests.head.repo.owner.login,
+	repo:context.github.pullRequests.head.repo.name,
+	sha:context.github.pullRequests.head.sha,
+	state:Status,
+	description:Description
+}).then(()=>context.github.response.send(Status))
+
+robot.log(context)
+
+});
+
+
+}
