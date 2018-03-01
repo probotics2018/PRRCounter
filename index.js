20604349ca3020e76ea3d5471cae159b5ee8f381
rycao18 <rycao18@gmail.com>
2018-03-01 14:12:20 -0600

    here

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const fs = require('fs');
-const numApproved = 2;
+const numApproved = 	;
 
 module.exports = robot => {
 	robot.on('pull_request.opened', async context => {
 	    console.log("SOMETHING IS HAPPENING");
 	    // Array of all pull requests and getting the first object in the array
 	    var usableContext = JSON.parse(JSON.stringify(context));
-	    var singlePRNum = usableContext["payload"]["pull_request"]["number"]
 	    var owner = usableContext["payload"]["pull_request"]["head"]["repo"]["owner"]["login"];
 	    var repo = usableContext["payload"]["pull_request"]["head"]["repo"]["name"];
 		
-		var commitArray = await context.github.pullRequests.getCommits({
-			"owner": owner, 
-			"repo": repo, 
-			"number": singlePRNum
-		});
-		commitArray = JSON.parse(JSON.stringify(commitArray));
-		var sha = commitArray["data"][0]["sha"];
+		var sha = usableContext["payload"]["pull_request"]["head"]["sha"]
 		if (sha == null) {
 			console.log("SHA IS NULL");
 		} else {
@@ -39,6 +32,7 @@ module.exports = robot => {
 		var owner = usableContext["payload"]["pull_request"]["head"]["repo"]["owner"]["login"];
 	    var repo = usableContext["payload"]["pull_request"]["head"]["repo"]["name"];
 
+	    console.log(JSON.stringify(context));
 	  	var reviewsArray = await context.github.pullRequests.getReviews({
 	  		"owner": owner,
 	  		"repo": repo,
@@ -51,7 +45,7 @@ module.exports = robot => {
   				approveReviews++;
   			}
   		}
-  		
+
 	  	var sha = usableContext["payload"]["review"]["commit_id"];
 	  	if (approveReviews >= numApproved) {
 	  		console.log("There are more than " + numApproved + " approved reviews");
